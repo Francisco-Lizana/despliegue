c155ccb0f26629c21ebc8b0e4d9c39bf01004183
Dresest <esteban.poblete.osses@gmail.com>
2023-05-12 00:26:24 -0400

    cambios en shared

--- src/app/modules/pages/pages.routing.ts
+++ src/app/modules/pages/pages.routing.ts
@@ -1,15 +1,66 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PagesComponent } from '../pages/pages.component';
+import { InicioComponent } from '../pages/inicio/inicio.component';
 import { AuthGuard } from 'src/app/core/_guards/auth/auth.guard';
-
+import { GraficaPieComponent } from './grafica-pie/grafica-pie.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PacientesComponent } from './pacientes/pacientes.component';
+import { CrearPacienteComponent } from './pacientes/crear-paciente/crear-paciente.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PagesComponent,
-    canActivate: [AuthGuard],
-    loadChildren: () => import('./child-routes.module').then(m => m.ChildRoutesModule),
+    children: [
+      {
+        path: 'inicio',
+        component: InicioComponent,
+        canActivate: [AuthGuard],
+        data: { titulo: 'Inicio' }
+      },
+      {
+        path: 'grafica1',
+        component: GraficaPieComponent,
+        canActivate: [AuthGuard],
+        data: { titulo: 'Grafica1' }
+      },
+      {
+        path: 'acccount-seting',
+        component: AccountSettingsComponent,
+        canActivate: [AuthGuard],
+        data: { titulo: 'Configuración' }
+      },
+      {
+        path: 'agenda',
+        component: AgendaComponent,
+        canActivate: [AuthGuard],
+        data: { titulo: 'Agenda' }
+      },
+      {
+        path: 'servicio',
+        component: ServiciosComponent,
+        canActivate: [AuthGuard],
+        data: { titulo: 'servicio' }
+      },
+      {
+        path: 'paciente',
+        component: PacientesComponent,
+        canActivate: [AuthGuard],
+        data: { titulo: 'pacientes' }
+        
+      },
+      {
+        path: 'addPaciente',
+        component: CrearPacienteComponent,
+        canActivate: [AuthGuard],
+        data: { titulo: 'Addpacientes' }
+        
+      },
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+    ]
   }
 ];
 
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,180 +1,53 @@
+import { RouterModule } from '@angular/router'
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatIconModule } from '@angular/material/icon';
-import { TableModule } from 'primeng/table';
-import { ToastModule } from 'primeng/toast';
-import { CalendarModule } from 'primeng/calendar';
-import { SliderModule } from 'primeng/slider';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { ContextMenuModule } from 'primeng/contextmenu';
-import { DialogModule } from 'primeng/dialog';
-import { ButtonModule } from 'primeng/button';
-import { DropdownModule } from 'primeng/dropdown';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { InputTextModule } from 'primeng/inputtext';
-import { StepsModule } from 'primeng/steps';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MessageService } from 'primeng/api';
 import { CommonModule } from '@angular/common';
-import {PanelMenuModule} from 'primeng/panelmenu';
-import {AvatarModule} from 'primeng/avatar';
-import {RouterModule} from '@angular/router'
-
-import { SplitButtonModule } from 'primeng/splitbutton';
-import { MatStepperModule } from '@angular/material/stepper';
-import { DynamicDialogModule } from 'primeng/dynamicdialog';
-import { BreadcrumbModule } from 'xng-breadcrumb';
-import { ToolbarModule } from 'primeng/toolbar';
-import { TooltipModule } from 'primeng/tooltip';
-import { EditorModule } from 'primeng/editor';
-import { CardModule } from 'primeng/card';
-import { CheckboxModule } from 'primeng/checkbox';
-import { TabViewModule } from 'primeng/tabview';
-import { MatDialogModule } from '@angular/material/dialog';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { MessagesModule } from 'primeng/messages';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MessageModule } from 'primeng/message';
-import { MatRadioModule } from '@angular/material/radio'; import { MatChipsModule } from '@angular/material/chips';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatTable, MatTableModule } from '@angular/material/table';
-import { ChipModule } from 'primeng/chip';
-import { ScrollPanelModule } from 'primeng/scrollpanel';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { AuthService } from 'src/app/core/_services/auth.service';
-import { ToastService } from 'src/app/core/_services/toast.service';
-import { BadgeModule } from 'primeng/badge';
+
 import { SortPipe } from 'src/app/core/_pipes/sort.pipe';
-import { ImageModule } from 'primeng/image';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
-
-
-
-
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
-declarations: [
+    declarations: [
+        SortPipe,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
 
-    SortPipe,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-
-],
-imports: [
-
-    RouterModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    TableModule,
-    ToastModule,
-    ToolbarModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    ImageModule,
-    InputTextModule,
-    StepsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    CommonModule,
-    PanelMenuModule,
-    AvatarModule,
-    CardModule,
-    CheckboxModule,
-    TabViewModule,
-    MatAutocompleteModule,
-    MatGridListModule,
-    ChipModule,
-    ScrollPanelModule,
-    MatProgressSpinnerModule,
-    BadgeModule,
-    MessageModule,
-    EditorModule,
-    MatDialogModule,
-    ProgressSpinnerModule,
-    ConfirmDialogModule,
-
-],
-providers: [SortPipe],
-bootstrap: [],
-exports: [
-    TableModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatExpansionModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatCardModule,
-    MatStepperModule,
-    MatInputModule,
-    MatCheckboxModule,
-    DropdownModule,
-    ImageModule,
-    MatIconModule,
-    CalendarModule,
-    SliderModule,
-    DialogModule,
-    ToolbarModule,
-    MultiSelectModule,
-    ContextMenuModule,
-    ButtonModule,
-    ToastModule,
-    InputTextModule,
-    ProgressBarModule,
-    StepsModule,
-    AvatarModule,
-    SplitButtonModule,
-    MatRadioModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    CommonModule,
-    PanelMenuModule,
-    DynamicDialogModule,
-    ToolbarModule,
-    TooltipModule,
-    EditorModule,
-    CardModule,
-    CheckboxModule,
-    TabViewModule,
-    MatDialogModule,
-    ProgressSpinnerModule,
-    ConfirmDialogModule,
-    MessagesModule,
-    MatTabsModule,
-    MatChipsModule,
-    MatAutocompleteModule,
-    MatGridListModule,
-    ChipModule,
-    ScrollPanelModule,
-    BadgeModule,
-    MessageModule,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-],
+    ],
+    imports: [
+        ToastModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        CommonModule,
+        MatAutocompleteModule,
+        MatListModule,
+    ],
+    providers: [SortPipe],
+    bootstrap: [],
+    exports: [
+        ToastModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        CommonModule,
+        MatListModule,
+        MatInputModule,
+        MatAutocompleteModule,
+        MatCardModule,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+    ],
 })
 export class SharedModule { }
